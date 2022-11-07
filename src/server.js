c29ce398ece5d4dc20f973fcee24501dd1807ca3
kvmoraes <karinevitoria.marinho@gmail.com>
2022-11-07 18:21:29 -0300

    feat: add healthchecker route

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const userRouter = require('./api/routes/user-routes');
 const productRouter = require('./api/routes/product-routes');
 const categoriesRouter = require('./api/routes/categories-routes');
 const filterRouter = require('./api/routes/filter-routes');
+const healthchecker = require('./api/routes/healthcheck');
 
 const app = express();
 const PORT = 3000;
@@ -14,6 +15,7 @@ app.use('/api', userRouter);
 app.use('/api', productRouter);
 app.use('/api', categoriesRouter);
 app.use('/api', filterRouter);
+app.use('/api', healthchecker);
 
 app.listen(PORT, () => {
 	console.log(`🚀 Server is up and running on port:${PORT}`);
